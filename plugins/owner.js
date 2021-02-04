17aa8663f2ef1ef39847f6bd8e3645757c368caf
CrzxaExe <78332709+CrzxaExe@users.noreply.github.com>
2021-02-04 17:57:00 +0700

    Add files via upload

---
+++ plugins/owner.js
@@ -0,0 +1,24 @@
+let handler = async (m, { conn }) => {
+  if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 1) {
+    conn.reply(m.chat, '+500 XP', m)  
+    global.DATABASE._data.users[m.sender].exp += 500
+    global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
+  } else conn.reply(m.chat, 'Anda sudah mengklaim klaim harian  hari ini', m)
+}
+handler.help = ['owner']
+handler.tags = ['xp']
+handler.command = /^(palkon|adam)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler
+
