7d1b8575e36e36bbbbb0f69590368169f422e664
Crzx <78332709+CrzxaExe@users.noreply.github.com>
2021-02-05 13:32:22 +0700

    Add files via upload

---
+++ plugins/ttslang.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn }) => {
+  conn.reply(m.chat, `
+  TTS language
+  id : Indonesia
+  en : English
+  je : Jepang
+  ru : Rusia
+  ne : India
+`.trim(), m)
+}
+handler.help = ['ttslang']
+handler.tags = ['tools']
+handler.command = /^ttslang/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler
+
