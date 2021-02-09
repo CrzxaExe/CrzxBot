561fb35e92a230206458d58b314802029dcc3b87
Crzx <78332709+CrzxaExe@users.noreply.github.com>
2021-02-09 14:53:33 +0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
 }
 handler.help = ['owner']
 handler.tags = ['owner']
-handler.command = /^(palkon|adam)$/i
+handler.command = /^(owner|adam)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
