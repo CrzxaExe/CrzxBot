ffa2724a24f3f6cc8c30c863068edb6a6853416d
Crzx <78332709+CrzxaExe@users.noreply.github.com>
2021-02-21 11:08:19 +0700

    Add files via upload

---
+++ plugins/anime.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn }) => {
+  try {
+	res = await fetchJson(`https://api.i-tech.id/anim/anime?key=pyjaPX-6Nzxgm-De2xYI-a5kUnj-KLtQom`, {method: 'get'})
+    buffer = await getBuffer(res.url)
+  } catch (e) {
+  }
+Nih
+`.trim()
+   conn.sendFile(m.chat, res, buffer)
+}
+handler.help = ['anime']
+handler.tags = ['anim']
+handler.command = /^anime$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler
+
